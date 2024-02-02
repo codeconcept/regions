c8c54dfdc03ae5c2dd6d49ba3d2b61c8ec4b53cc
Code Concept <contact@codeconcept.com>
2024-02-02 14:24:51 +0100

    Avoiding writing a signal inside an effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RegionsService } from './services/regions.service';
 import { Subscription, tap } from 'rxjs';
 import Region from './interfaces/region';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -19,15 +27,41 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscription = this.regionsService
       .getRegions()
-      .pipe(tap((data) => this.regionsSig.set(data)))
+      .pipe(
+        tap((data) => {
+          this.regions = data;
+          this.regionsSig.set(data);
+        })
+      )
       .subscribe();
   }
   title = 'regions';
   regionsSig = signal<Region[]>([]);
+  regions: Region[] = [];
   regionsService = inject(RegionsService);
   selectedRegionSig = this.regionsService.selectedRegionSig;
   subscription!: Subscription;
-  regionDepartmentsSig = this.regionsService.regionDepartments
+  regionDepartmentsSig = this.regionsService.regionDepartments;
+  filterBySig = signal<string>('');
+
+  filterEffect = effect(() => {
+    console.log(this.filterBySig().toLocaleLowerCase());
+    if (this.filterBySig() === '') {
+      this.resetRegions();
+      return;
+    }
+    
+    this.regions = this.regions.filter((reg) =>
+    reg.Name.toLocaleLowerCase().startsWith(
+      this.filterBySig().toLocaleLowerCase()
+      )
+      );
+    console.log({ filteredRegions: this.regions });
+  });
+
+  resetRegions() {
+    this.regions = this.regionsSig();
+  }
 
   selectRegion(region: Region) {
     this.selectedRegionSig.set(region);
